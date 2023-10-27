56725cdc5704c3f0e439a9c3220dbc690855038a
Pratik Sakaria <pr711119@dal.ca>
2023-10-27 15:33:42 -0300

    feat: Consumed additional GitHub APIs
    -Added Statistics of Languages used in Repo
    -Added Statistics of No. of Commits by individual contributor
    -Added statistics of weekly commit history per day
    -Added pie chart for displaying Language distribution
    -Added bar graph for displaying individual contributors commit
    -Handled source code received for repository and stored in an external txt file.

---
+++ src/main/java/com/taim/conduire/controller/LLMController.java
@@ -0,0 +1,246 @@
+package com.taim.conduire.controller;
+
+import com.taim.conduire.model.CodeFrequencyStat;
+import com.taim.conduire.service.JSONUtils;
+import com.taim.conduire.service.LLMService;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+
+import javax.crypto.spec.PSource;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+
+import static org.jfree.chart.ChartFactory.createBarChart;
+
+@RestController
+public class LLMController {
+
+    private final LLMService llmService;
+
+    private JSONUtils jsonUtils;
+
+    @Value("${github.api.url}")
+    private String githubApiUrl;
+
+    @Value("${github.repository.owner}")
+    private String owner;
+
+    @Value("${github.repository.name}")
+    private String repo;
+
+    //private String[] fileTypes = {".jpg",".class",".png","svg", "docx", "exe", "dll", "jar", "gif"};
+
+    public LLMController(LLMService llmService) {
+        this.llmService = llmService;
+    }
+
+    @GetMapping("/repository/languages")
+    public Map<String, Integer> getRepositoryLanguages() {
+        return llmService.getRepositoryLanguages();
+    }
+
+    @GetMapping("/repository/code-frequency")
+    public List<List<Integer>> getRepositoryCodeFrequency() throws IOException {
+
+        return llmService.getRepositoryCodeFrequency();
+    }
+
+    @GetMapping("repository/punch")
+    public List<List<Integer>> getRepositoryPunchCard() throws IOException {
+
+        return llmService.getRepositoryPunchCard();
+    }
+
+    @GetMapping("repository/punchtest")
+    public int[] getRepositoryPunchCardtest() throws IOException {
+
+        return llmService.getRepositoryPunchCardtest();
+    }
+
+    @GetMapping("repository/chart")
+    public ResponseEntity<byte[]> getLanguageChart() throws IOException{
+        byte[] chartImage = llmService.generatePieChart(llmService.getRepositoryLanguages());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(chartImage, headers, HttpStatus.OK);
+
+    }
+    @GetMapping("/download/{owner}/{repoName}")
+    public ResponseEntity<byte[]> downloadRepositoryCode(
+            @PathVariable String owner,
+            @PathVariable String repoName) {
+        byte[] code = llmService .downloadRepositoryCode(owner, repoName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", repoName + ".zip");
+        return new ResponseEntity<>(code, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/repo")
+    public String getRepoData() {
+        return llmService.getRepoData();
+    }
+
+    @GetMapping("/test")
+    public String getTest(){
+        return llmService.getHelloWorld();
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/repository/content")
+    public void getRepoContent(){
+        String basePath = "";
+        ResponseEntity<String> response = llmService.getRepositoryContents(owner,repo);
+        System.out.println(response);
+
+        if(response!=null && response.getBody()!=null) {
+            List<Map<String, Object>> contents = JSONUtils.parseJSONResponse(response.getBody());
+
+            for (Map<String, Object> item : contents) {
+                String type = (String) item.get("type");
+                String name = (String) item.get("name");
+                String path = (String) item.get("path");
+                System.out.println(type + "***" + name + "***" + path);
+
+                if ("file".equals(type) && !path.toLowerCase().contains("jpg") && !path.toLowerCase().contains("png") && !path.toLowerCase().contains("svg") && !path.toLowerCase().contains("class")
+                        && !path.toLowerCase().contains("docx") && !path.toLowerCase().contains("exe") && !path.toLowerCase().contains("dll")
+                        && !path.toLowerCase().contains("jar") && !path.toLowerCase().contains("gif") && !path.toLowerCase().contains("css") && !path.toLowerCase().contains("html")) {
+                    processFile(owner, repo, path, basePath);
+                }
+                else if ("dir".equals(type)) {
+                    processDirectory(owner, repo, path, basePath);
+                }
+            }
+        }
+    }
+
+    private void processDirectory(String owner, String repo, String dirPath, String basePath) {
+        System.out.println("WE IN PROCESSDIRECTORY");
+        ResponseEntity<String> response = llmService.getRepositoryContents(owner,repo,dirPath);
+        System.out.println(response.getBody());
+        List<Map<String, Object>> contents = JSONUtils.parseJSONResponse(response.getBody());
+        System.out.println(contents);
+
+        if (StringUtils.hasText(basePath)) {
+            basePath = basePath + File.separator + dirPath;
+        } else {
+            basePath = dirPath;
+        }
+
+        for (Map<String, Object> item : contents) {
+            String type = (String) item.get("type");
+            String path = (String) item.get("path");
+
+            if ("file".equals(type) && !path.toLowerCase().contains("jpg") && !path.toLowerCase().contains("png") && !path.toLowerCase().contains("svg") && !path.toLowerCase().contains("class")
+                    && !path.toLowerCase().contains("docx") && !path.toLowerCase().contains("exe") && !path.toLowerCase().contains("dll")
+                    && !path.toLowerCase().contains("jar") && !path.toLowerCase().contains("gif") && !path.toLowerCase().contains("css") && !path.toLowerCase().contains("html")) {
+                processFile(owner, repo, path, basePath);
+            } else if ("dir".equals(type)) {
+                processDirectory(owner, repo, path, basePath);
+            }
+        }
+        System.out.println("WE OUT PROCESSFILE");
+    }
+
+    private void processFile(String owner, String repo, String filePath, String basePath) {
+        System.out.println("WE IN PROCESSFILE");
+        filePath = filePath.trim().replaceAll(" ","%20");
+        System.out.println(filePath);
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println("Current Working Directory: " + currentDirectory);
+        ResponseEntity<String> response = llmService.getFileContent(owner, repo, filePath);
+        System.out.println(response.getBody());
+        String content = response.getBody();
+        String title = filePath;
+        //Map<String, Object> file = JSONUtils.parseJSONResponseAsMap(response.getBody());
+        /*System.out.println(file);
+
+        String title = (String) file.get("name");
+        String content = (String) file.get("content");
+        System.out.println("Name : " + title);
+        System.out.println("Content : " + content);*/
+
+        if (StringUtils.hasText(basePath)) {
+            /*String outputDir = "output" + File.separator + basePath;
+            new File(outputDir).mkdirs();
+            try (FileWriter fileWriter = new FileWriter(outputDir + File.separator + title)) {
+                fileWriter.write(title + "\n");
+                fileWriter.write(new String(Base64.getDecoder().decode(content)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }*/
+
+            String fileName = "output.txt"; // Replace with the desired file name
+
+            try {
+                // Create a FileWriter with append mode (true) to append to an existing file
+                // If the file does not exist, it will be created
+                FileWriter fileWriter = new FileWriter(fileName, true);
+
+                // Create a PrintWriter for writing text to the file
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+
+                // Write the content to the file
+                printWriter.println("File Name: "+title);
+                printWriter.println();
+                printWriter.println("Content:");
+                printWriter.println(content);
+                printWriter.println();
+
+                // Close the PrintWriter and FileWriter
+                printWriter.close();
+                fileWriter.close();
+
+                System.out.println("Content has been written to the file.");
+            } catch (IOException e) {
+                System.err.println("An error occurred: " + e.getMessage());
+            }
+        }
+        System.out.println("WE OUT PROCESSFILE");
+    }
+
+    @GetMapping("repository/contributors")
+    public ResponseEntity<byte[]> getContributors() throws IOException {
+
+        List<Map<String, Object>> contributors = llmService.getRepoContributors();
+        // Generate the bar chart
+        JFreeChart barChart = llmService.generateBarChart(contributors);
+
+        // Generate chart image as a byte array
+        byte[] chartImage = generateBarChartImage(barChart);
+
+        // Return the chart image as a response with content type 'image/png'
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(chartImage);
+    }
+
+    private byte[] generateBarChartImage(JFreeChart chart) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ChartUtilities.writeChartAsPNG(byteArrayOutputStream, chart, 800, 400);
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+}
---
+++ src/main/java/com/taim/conduire/service/LLMService.java
@@ -0,0 +1,300 @@
+package com.taim.conduire.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class LLMService {
+
+    @Value("${github.api.url}")
+    private String githubApiUrl;
+
+    @Value("${github.repository.owner}")
+    private String owner;
+
+    @Value("${github.repository.name}")
+    private String repo;
+
+    @Value("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")
+    private String accessToken;
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public LLMService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public Map<String, Integer> getRepositoryLanguages() {
+        String apiUrl = String.format("%s/repos/%s/%s/languages", githubApiUrl, owner, repo);
+        return restTemplate.getForObject(apiUrl, Map.class);
+    }
+
+    public List<List<Integer>> getRepositoryCodeFrequency() throws IOException {
+        String apiUrl = String.format("%s/repos/%s/%s/stats/code_frequency", githubApiUrl, owner, repo);
+        System.out.println(apiUrl);
+        String apiResponse = restTemplate.getForObject(apiUrl, String.class);
+        List<List<Integer>> codeFrequencyStats = objectMapper.readValue(apiResponse, objectMapper.getTypeFactory().constructCollectionType(List.class, List.class));
+        return codeFrequencyStats;
+    }
+
+    public List<List<Integer>> getRepositoryPunchCard() throws IOException {
+        String apiUrl = String.format("%s/repos/%s/%s/stats/punch_card", githubApiUrl, owner, repo);
+        System.out.println(apiUrl);
+        String apiResponse = restTemplate.getForObject(apiUrl, String.class);
+        List<List<Integer>> repoPunchCard = objectMapper.readValue(apiResponse, objectMapper.getTypeFactory().constructCollectionType(List.class, List.class));
+        computeWeeklyCommits(repoPunchCard);
+        return repoPunchCard;
+    }
+
+    public int[] getRepositoryPunchCardtest() throws IOException {
+        String apiUrl = String.format("%s/repos/%s/%s/stats/punch_card", githubApiUrl, owner, repo);
+        System.out.println(apiUrl);
+        String apiResponse = restTemplate.getForObject(apiUrl, String.class);
+        List<List<Integer>> repoPunchCard = objectMapper.readValue(apiResponse, objectMapper.getTypeFactory().constructCollectionType(List.class, List.class));
+        return computeWeeklyCommits(repoPunchCard);
+    }
+
+    public byte[] generatePieChart(Map<String,Integer> data) throws IOException{
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        for (Map.Entry<String, Integer> entry : data.entrySet()) {
+            dataset.setValue(entry.getKey(), entry.getValue());
+        }
+
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Your Pie Chart Title",
+                dataset,
+                true,
+                true,
+                false
+        );
+
+        PiePlot plot = (PiePlot) chart.getPlot();
+        //plot.setSection(0000.35);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ChartUtilities.writeChartAsPNG(outputStream, chart, 400, 300);
+        return outputStream.toByteArray();
+    }
+
+    public JFreeChart generateBarChart(List<Map<String,Object>> contributors) throws IOException{
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (Map<String, Object> contributor : contributors) {
+            String contributorName = (String) contributor.get("login");
+            int contributions = (Integer) contributor.get("contributions");
+            dataset.addValue(contributions, "Contributions", contributorName);
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Contributions of each developer",
+                "Contributor",
+                "Contributions",
+                dataset,
+                PlotOrientation.VERTICAL,
+                false,
+                true,
+                false
+        );
+
+        return chart;
+    }
+
+    public int[] computeWeeklyCommits(List<List<Integer>> repoPunchCard){
+        int[] weeklyCount = new int[7];
+        int count = 0;
+        int i=0,x=0;
+        //Sunday
+        while(i<=23)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        x++;
+        count=0;
+        //System.out.println("Value of i is: " + i);
+        //Monday
+        while(i<=46)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        x++;
+        count=0;
+        //System.out.println("Value of i is: " + i);
+        //Tuesday
+        while(i<=69)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        x++;
+        count=0;
+        //Wednesday
+        while(i<=92)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        x++;
+        count=0;
+        //Thursday
+        while(i<=115)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        x++;
+        count=0;
+        //Friday
+        while(i<=138)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        x++;
+        count=0;
+        //Saturday
+        while(i<=161)
+        {
+            List<Integer> tempList= repoPunchCard.get(i);
+            count = count + tempList.get(2);
+            i++;
+        }
+        weeklyCount[x]=count;
+        for(int m=0; m<weeklyCount.length;m++)
+            System.out.println("No. of Commits on day "+m+": " + weeklyCount[m]);
+
+        return weeklyCount;
+    }
+
+    public byte[] downloadRepositoryCode(String owner, String repoName) {
+        String downloadUrl = githubApiUrl + "/repos/" + owner + "/" + repoName + "/zipball";
+        HttpHeaders headers = new HttpHeaders();
+        //headers.setBearerAuth(apiToken);
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<byte[]> responseEntity = restTemplate.exchange(
+                downloadUrl,
+                HttpMethod.GET,
+                requestEntity,
+                byte[].class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return responseEntity.getBody();
+        } else {
+            throw new RuntimeException("Failed to download repository code. Status code: " + responseEntity.getStatusCodeValue());
+        }
+    }
+
+    public String getHelloWorld(){
+        String uri = "http://localhost:8080/hello";
+        RestTemplate restTemplate1 = new RestTemplate();
+        return restTemplate1.getForObject(uri,String.class);
+    }
+
+    public String getRepoData() {
+        String apiUrl = String.format("%s/repos/%s/%s", githubApiUrl, owner, repo);
+        //String apiUrl = githubApiUrl + "/repos/" + owner + "/" + repoName;
+        return restTemplate.getForObject(apiUrl, String.class);
+    }
+
+    public List<Map<String,Object>> getRepoContributors(){
+        String apiUrl = String.format("%s/repos/%s/%s/contributors", githubApiUrl, owner, repo);
+        return restTemplate.getForObject(apiUrl, List.class);
+    }
+
+    public ResponseEntity<String> getRepositoryContents(String owner, String repo, String path) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        //headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        //String apiUrl = String.format("%s/repos/%s/%s", githubApiUrl, owner, repo);
+        URI uri = URI.create(githubApiUrl + "/repos/" + owner + "/" + repo + "/contents" + "/" + path);
+
+        RequestEntity<?> requestEntity = RequestEntity.get(uri).headers(headers).build();
+        return restTemplate.exchange(requestEntity, String.class);
+    }
+
+    public ResponseEntity<String> getRepositoryContents(String owner, String repo) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        //headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        //String apiUrl = String.format("%s/repos/%s/%s", githubApiUrl, owner, repo);
+        URI uri = URI.create(githubApiUrl + "/repos/" + owner + "/" + repo + "/contents");
+
+        RequestEntity<?> requestEntity = RequestEntity.get(uri).headers(headers).build();
+        return restTemplate.exchange(requestEntity, String.class);
+    }
+
+    public ResponseEntity<String> getFileContent(String owner, String repo, String filePath) {
+        HttpHeaders headers = createHeaders();
+
+        URI uri = URI.create(githubApiUrl + "/repos/" + owner + "/" + repo + "/contents/" + filePath);
+
+        RequestEntity<?> requestEntity = RequestEntity.get(uri).headers(headers).build();
+        ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
+
+        if (response.getStatusCodeValue() == 200) {
+            // The response contains base64-encoded content, so decode it
+            String content = response.getBody();
+            System.out.println("RESPONSE 200");
+            System.out.println(content);
+            //content = content.substring(content.indexOf("content")+9,content.indexOf("encoding")-3);
+            content = JSONUtils.parseJSONResponseAsTree(content);
+            content = content.replaceAll("\\s","");
+            System.out.println(content);
+            String decodedContent = new String(Base64.getDecoder().decode(content), StandardCharsets.UTF_8);
+            System.out.println("WE REACHED HERE ATLEAST!!!" + decodedContent);
+            response = ResponseEntity.ok(decodedContent);
+        }
+
+        return response;
+    }
+
+    private HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        //headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return headers;
+    }
+
+}
