5148124764fa0422a763a056a4fc78bf110c8e33
Pratik Sakaria <pr711119@dal.ca>
2023-11-19 22:26:42 -0400

    Resolving Merge Conflicts before pushing Refactoring Code

--- src/main/java/com/taim/conduire/service/InsightsService.java
+++ src/main/java/com/taim/conduire/service/InsightsService.java
@@ -1,18 +1,11 @@
 package com.taim.conduire.service;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.taim.conduire.constants.ConstantCodes;
 import com.taim.conduire.domain.RepoData;
-import com.taim.conduire.domain.UserData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public interface InsightsService {
 
@@ -20,52 +13,7 @@ public interface InsightsService {
 
     void showAvailableAPIHits(HttpHeaders responseHeaders);
 
-    @Autowired
-    private RestTemplate restTemplate;
+    Map<String, List<String>> getRepositoryReviewComments(RepoData repoData);
 
-    public Map<String, List<String>> getRepositoryReviewComments(RepoData repoData) {
-        String apiUrl = String.format("%s/repos/%s", GITHUB_API_URL, repoData.getName());
-        System.out.println("apiUrl: " + apiUrl);
-
-        UserData userData = userDataService.getOne(repoData.getUserId());
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", userData.getUserAccessToken());
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
-
-        String jsonObjectString = response.getBody();
-        System.out.println("jsonObjectString: " + jsonObjectString);
-        Gson gson = new Gson();
-        JsonObject repoJsonObject = gson.fromJson(jsonObjectString, JsonObject.class);
-        System.out.println("repoJsonObject: " + repoJsonObject);
-        ResponseEntity<String> prReviewResposne = null;
-
-        if(repoJsonObject.get("fork").getAsBoolean()){
-            JsonObject repoSourceJsonObject = repoJsonObject.get("source").getAsJsonObject();
-            String repoSourceURL = repoSourceJsonObject.get("url").getAsString();
-            String prReviewCommentsURL = String.format(repoSourceURL + GITHUB_PULLS + GITHUB_COMMENTS);
-            // TODO: extract the same part form if-else
-            prReviewResposne = restTemplate.exchange(prReviewCommentsURL, HttpMethod.GET, entity, String.class);
-        } else {
-            String prReviewCommentsURL = String.format(apiUrl + GITHUB_PULLS + GITHUB_COMMENTS);
-            prReviewResposne = restTemplate.exchange(prReviewCommentsURL, HttpMethod.GET, entity, String.class);
-        }
-        String prReviewJsonArrayString = prReviewResposne.getBody();
-        JsonArray prReviewJsonArray = gson.fromJson(prReviewJsonArrayString, JsonArray.class);
-        System.out.println("prReviewJsonArray: " + prReviewJsonArray.size());
-        Map<String, List<String>> reviewerComments = new HashMap<>();
-
-        for (JsonElement element : prReviewJsonArray) {
-            // TODO: Early return
-            if (element.isJsonObject()) {
-                JsonObject reviewCommentObject = element.getAsJsonObject();
-                String reviewer = reviewCommentObject.get("user").getAsJsonObject().get("login").getAsString();
-                String reviewerComment = reviewCommentObject.get("body").getAsString();
-                reviewerComments.computeIfAbsent(reviewer, k -> new ArrayList<>()).add(reviewerComment);
-            }
-        }
-        System.out.println("reviewerComments: " + reviewerComments.size());
-        System.out.println("reviewerComments: " + reviewerComments.toString());
-        return reviewerComments;
-    }
+    String getCodeQualityEnhancementsInsights(RepoData repoData);
 }
