97733ce825d4734ce87afe78ade5fe418533ce1f
Sameer Sanjay Amesara <sm527221@dal.ca>
2023-11-26 23:26:53 -0400

    Update LLMServiceImplTest.java

--- src/test/java/com/taim/conduire/service/impl/LLMServiceImplTest.java
+++ src/test/java/com/taim/conduire/service/impl/LLMServiceImplTest.java
@@ -42,6 +42,7 @@ public class LLMServiceImplTest {
         // Mock getTypeFactory() behavior
         when(objectMapper.getTypeFactory()).thenReturn(typeFactory);
     }
+    
     @Test
     void testGetRepositoryPunchCardtest() throws IOException {
         // Mocked API response and object mapper behavior
@@ -61,21 +62,38 @@ public class LLMServiceImplTest {
     }
 
     @Test
-    public void testComputeWeeklyCommits() {
+    public void testComputeWeeklyCommitsWithNullInput() {
+        int[] expectedResult = new int[7]; // All initialized to 0 by default
+
+        int[] result = llmService.computeWeeklyCommits(null);
+
+        assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testComputeWeeklyCommitsWithEmptyInput() {
+        List<List<Integer>> emptyRepoPunchCard = new ArrayList<>();
+        int[] expectedResult = new int[7]; // All initialized to 0 by default
+
+        int[] result = llmService.computeWeeklyCommits(emptyRepoPunchCard);
+
+        assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testComputeWeeklyCommitsWithData() {
         // Mock repository punch card data for testing
         List<List<Integer>> repoPunchCard = new ArrayList<>();
-        for (int i = 0; i <= 161; i++) {
-            List<Integer> tempList = Arrays.asList(0, 0, 0); // Creating mock data [0, 0, 0]
+        for (int i = 0; i < 168; i++) { // 168 entries for 7 days (7 * 24)
+            List<Integer> tempList = Arrays.asList(0, 0, i); // [0, 0, 0], [0, 0, 1], [0, 0, 2], ...
             repoPunchCard.add(tempList);
         }
 
-        // Call the method
         int[] result = llmService.computeWeeklyCommits(repoPunchCard);
 
-        // Expected result: All days have zero commits (due to mock data)
-        int[] expectedResult = new int[7]; // All initialized to 0 by default
+        // Expected result: Total of all third elements (index 2) in each sublist for 7 days
+        int[] expectedResult = {276, 852, 1428, 2004, 2580, 3156, 3732}; // Values are calculated manually
 
-        // Assertions
         assertArrayEquals(expectedResult, result);
     }
 }
